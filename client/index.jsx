94fa8611bedc7455c68b8af7738780b5f92ba8cd
Doug Riblet <smashzen85@gmail.com>
2017-05-20 18:45:04 -0500

    create components

--- client/index.jsx
+++ client/index.jsx
@@ -1,10 +1,10 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
-import App from './components/App'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import reducer from './reducers/root_reducer'
-import logger from 'redux-logger'
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducer from './reducers/root_reducer';
+import logger from 'redux-logger';
 import promise from 'redux-promise';
 
 const store = createStore(reducer, {}, applyMiddleware(logger, promise));
